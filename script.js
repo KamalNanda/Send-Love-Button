8bc9242e6fb9c238cbb52fff55c30168c9f9f9e7
Kamal Nanda <kamalnanda45@gmail.com>
2020-05-18 11:33:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('nameHolder').innerText = name
+function heartFly(){
+	var holder = document.getElementById('heart-holder')
+	for(var i = 0 ; i< 15 ; i++){
+		setTimeout(function(){
+			var heart = document.createElement('span')
+			heart.style.position = "absolute"
+			heart.classList.add('heart')
+			heart.innerText = "❤"
+			heart.style.left = Math.random()*100 + '%'
+			heart.style.bottom = Math.random()*100 + 50+ '%'
+			heart.style.fontSize = Math.random() * 20 + 5 + 'px'
+			heart.style.animation = "fly 3s linear forward"
+			heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+			holder.appendChild(heart)
+			setTimeout(function(){
+				heart.remove()
+			} , 3000)
+		} , i*100)
+	}
+}
